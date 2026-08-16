750f8449ad84d13ce8adfb5e91aa65fdddc4201b
JngoCreates <jngo102@aim.com>
2026-08-16 10:32:17 -0400

    Fix single clip in RandomAudioPlayer not playing

--- game/actors/random_audio_player.gd
+++ game/actors/random_audio_player.gd
@@ -5,6 +5,7 @@ class_name RandomAudioPlayer extends AudioStreamPlayer3D
 var _previous_clip: AudioStream
 
 func play_random(from_position: float = 0, pitch_min: float = 1, pitch_max: float = 1) -> void:
+	stream = audio_clips.pick_random()
 	while len(audio_clips) > 1 and stream == _previous_clip:
 		stream = audio_clips.pick_random()
 	pitch_scale = randf_range(pitch_min, pitch_max)
